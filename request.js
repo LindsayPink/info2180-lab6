b89c32f52493e5d0101ffb763d60869866161ac7
Lindsay Pink <lindsay.pink@mymona.uwi.edu>
2018-11-12 19:53:35 +0000

    exercise 2: alert definition

---
+++ request.js
@@ -0,0 +1,24 @@
+window.onload = function () {
+    "use strict";
+    let btn = document.getElementById("search");
+    btn.addEventListener("click", exec);
+};
+        
+function exec () {
+    let httpRequest = new XMLHttpRequest();
+    let url = "https://info2180-lab6-lindsaypink.c9users.io/request.php/request.php?q=definition";
+    httpRequest.onreadystatechange = function() {
+        if (httpRequest.readyState === 4) {
+            if (httpRequest.status === 200) {
+                let response = (httpRequest.responseText);
+                let div = document.createElement("div"); //used to remove html tags from php response
+                div.innerHTML = response; //used to remove html tags from php response
+                alert(div.innerText);
+            } else {
+                alert("Problem with request.");
+            }
+        }
+    };
+    httpRequest.open("GET", url);
+    httpRequest.send();
+}
