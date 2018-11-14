18e5da37a121f1f27c97d60b0d10c87490591a93
Lindsay Pink <lindsay.pink@mymona.uwi.edu>
2018-11-14 03:04:22 +0000

    everything since the last commit

--- request.js
+++ request.js
@@ -34,5 +34,43 @@ function exec () {
 }
 
 function showAll () {
-   
+    let httpRequest = new XMLHttpRequest();
+    let url = "https://info2180-lab6-lindsaypink.c9users.io/request.php/request.php?q=&all=true";
+    httpRequest.onreadystatechange = function() {
+        if (httpRequest.readyState === 4) {
+            if (httpRequest.status === 200) {
+                let response = (httpRequest.responseText);
+                document.getElementById("result").innerHTML = response;
+            } else {
+                alert("Problem with request. Try again");
+            }
+        }
+    };
+    httpRequest.open("GET", url);
+    httpRequest.send();
+    
+    function loadDoc() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      myFunction(this);
+    }
+  };
+  xhttp.open("GET", "request.xml", true);
+  xhttp.send();
+}
+function myFunction(xml) {
+  let xmlDoc = xml.responseXML;
+  let oList = document.createElement("ol");
+  
+  let x = xmlDoc.getElementsByTagName("definition");
+  for (let i = 0; i < x.length; i++) { 
+    oList.appendChild(document.createElement("h3"));
+    x[i].getElementsByTagName("ARTIST")[0].childNodes[0].nodeValue +
+
+    x[i].getElementsByTagName("TITLE")[0].childNodes[0].nodeValue +
+
+  }
+  document.getElementById("demo").innerHTML = table;
+}
 }
